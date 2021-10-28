dfe2d19d3c743318ca708648709c748029cbf963
Zeyad <zeyad.ta01@gmail.com>
2021-10-29 00:47:31 +0200

    Variables

---
+++ variables.php
@@ -0,0 +1,36 @@
+<h1>hello zeyad</h1>
+
+<h1>
+<?php
+  echo 'Hello World';
+?>
+</h1>
+
+<?php
+  // print 'Hello World';
+
+  // single line comment
+  # Also single
+
+  /*date_sub
+  date_sub
+  sda
+  */
+
+  #VARIABLES
+  /*
+
+  */
+
+
+  $name1 = 'Zeyad';
+  $name2 = 'Tarek';
+  $fName = "$name1 $name2";
+
+  $string3 = 'They\'re here';
+
+  define("MYNAME","Zeyad Tarek Khairy");
+  echo MYNAME;
+
+
+?>
