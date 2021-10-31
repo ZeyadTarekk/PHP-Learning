723f2a444ed05a2e9d7df6da6fc2b3018a9e9cc3
Zeyad Tarek <zeyad.aly01@eng-st.cu.edu.eg>
2021-10-31 23:42:28 +0200

    foreach

--- loops.php
+++ loops.php
@@ -1,12 +1,32 @@
 <?php
   for($i = 0 ; $i<10 ; $i++) {
-    echo "Number: " .$i+1 . "<br>";
+    echo "Number: " . $i+1 . "<br>";
   }
 
   $i = 0 ;
   while($i <10) {
-    echo "Number: " .$i+1 . "<br>";
+    echo "Number: " . $i+1 . "<br>";
     $i++;
+  }
+
+
+  $i = 0 ;
+  do {
+    echo "Number: ". $i+1 ."<br>";
+    $i++;
+  }
+  while($i<10);
+
+  $people = ["Zeyad" , "Tarek", "Ahmed" , "Mohamed" , "Sherif"];
+  print_r($people);
+  echo "<br>";
+  foreach($people as $elem) {
+    echo $elem . "<br>";
+  }
+
+  $peoplev2 = array("zeyad" => "zizo@gmaik.com" , "tarek" => "tarek@ossn.com", "Ahmed" => "ahmed@yahoo.com");
+  foreach($peoplev2 as $elem2 => $email) {
+    echo $elem2 ." " . $email . "<br>";
   }
 
 ?>
