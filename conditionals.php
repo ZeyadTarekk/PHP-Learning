a4f9d94ae31ea6c2f0d38c06c8b79e86bee4e2a0
Zeyad Tarek <zeyad.aly01@eng-st.cu.edu.eg>
2021-11-05 00:41:31 +0200

    L;ogical Operators

--- conditionals.php
+++ conditionals.php
@@ -3,13 +3,23 @@
   echo (true==="1")?"TRUE<br>":"FALSE<br>";
 
 
-  $num = 5;
+  $num = 11;
 
-  if($num == 5) {
-    echo "5 Passed!<br>";
-  } elseif($num == 6) {
-    echo "6 Passed!<br>";
-  } else {
-    echo "Didn't pass <br>";
+  // if($num == 5) {
+  //   echo "5 Passed!<br>";
+  // } elseif($num == 6) {
+  //   echo "6 Passed!<br>";
+  // } else {
+  //   echo "Didn't pass <br>";
+  // }
+
+  if($num > 4 AND $num < 10) {
+    echo "5 Passed";
   }
+
+  /* Logical operator
+    AND => &&
+    OR => ||
+    xor
+  */
 ?>
