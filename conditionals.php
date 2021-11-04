97b86b85755f168814f5873653dd9ac81958e437
Zeyad Tarek <zeyad.aly01@eng-st.cu.edu.eg>
2021-11-05 00:26:42 +0200

    Conditionals 1

---
+++ conditionals.php
@@ -0,0 +1,15 @@
+<?php
+  echo "Hello Zeyad <br>";
+  echo (true==="1")?"TRUE<br>":"FALSE<br>";
+
+
+  $num = 5;
+
+  if($num == 5) {
+    echo "5 Passed!<br>";
+  } elseif($num == 6) {
+    echo "6 Passed!<br>";
+  } else {
+    echo "Didn't pass <br>";
+  }
+?>
