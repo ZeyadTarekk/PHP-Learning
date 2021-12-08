4d4ce0373accf3c060a199db391d141488a159b1
Zeyad Tarek <zeyad.ta01@gmail.com>
2021-12-09 01:27:48 +0200

    Explode function

--- Website5/index.php
+++ Website5/index.php
@@ -22,35 +22,42 @@
 
 	mysqli_close($conn);
 	// print_r($pizzas1);
+	// print_r(explode(',',$pizzas1[0]['ingredients']));
 
 ?>
 
+
+
 <!DOCTYPE html>
 <html>
-	
-	<?php include('templates/header.php'); ?>
 
-	<h4 class="center grey-text">Pizzas</h4>
-	<div class="container">
-		<div class="row">
-			<?php foreach($pizzas1 as $pizza) { ?>
+<?php include('templates/header.php'); ?>
 
-				<div class="col s6 md3">
-					<div class="card z-depth-0">
-						<div class="card-content center">
-							<h6><?php echo htmlspecialchars($pizza['title']); ?></h6>
-							<div><?php echo htmlspecialchars($pizza['ingredients']); ?></div>
-						</div>
-						<div class="card-action right-align">
-							<a href="#" class="brand-text">more info</a>
-						</div>
-					</div>
-				</div>
+<h4 class="center grey-text">Pizzas</h4>
+<div class="container">
+  <div class="row">
+    <?php foreach($pizzas1 as $pizza) : ?>
 
-			<?php  } ?>
-		</div>
-	</div>
+    <div class="col s6 md3">
+      <div class="card z-depth-0">
+        <div class="card-content center">
+          <h6><?php echo htmlspecialchars($pizza['title']); ?></h6>
+          <ul>
+            <?php foreach(explode(',',$pizza['ingredients']) as $ingred) :?>
+            <li><?php echo htmlspecialchars($ingred) ?></li>
+            <?php endforeach; ?>
+          </ul>
+        </div>
+        <div class="card-action right-align">
+          <a href="#" class="brand-text">more info</a>
+        </div>
+      </div>
+    </div>
 
-	<?php include('templates/footer.php'); ?>
+    <?php  endforeach; ?>
+  </div>
+</div>
+
+<?php include('templates/footer.php'); ?>
 
 </html>
